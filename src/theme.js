5ac55478f2724edd37dabc6227beae287bda1abf
RobertoJr2001 <junionascimento2001@gmail.com>
2021-06-01 00:28:06 -0300

    feat: Home page, reset styles and default theme

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    primary: '#6200ee',
+    backgroun: '#00000014',
+    text: '#000000B3',
+    sucess: '135E01',
+  },
+  fonts: {
+    regular: 'Roboto, sans-serif',
+  },
+};
