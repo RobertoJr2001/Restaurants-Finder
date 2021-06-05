716b64df53754c58cf3f4725b521bb82e5b42cdd
RobertoJr2001 <junionascimento2001@gmail.com>
2021-06-04 21:47:23 -0300

    feat: create carousel

---
+++ src/components/ImageCard/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { Card, Title } from './style';
+
+export default ({ photo, title }) => <Card photo={photo}><Title>{ title }</Title></Card>
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,28 +1,54 @@
 import React, { useState } from 'react';
+import Slider from 'react-slick';
 import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
+import restaurant from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
 
-import { Container, Search } from './styles'
+import { Wrapper, Container, Carousel, Search, Logo, Map, CarouselTitle } from './styles'
 
 export default () => {
   const [ inputValue, setInputValue] = useState('');
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    adaptiveHeight: true,
+  };
  
- return (
-    <Container>
-      <Search>
-        <img src={logo} alt="Restaurant Logo" />
-        <TextField
-            label='Search'
-            outlined
-            // onTrailingIconSelect={() => this.setState({value: ''})}
-            // trailingIcon={<MaterialIcon role="button" icon="delete"/>}
-          >
-            <Input
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)} />
-          </TextField>
-      </Search>
-    </Container>
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Restaurant Logo" />
+          <TextField
+              label='Search restaurants'
+              outlined
+              trailingIcon={<MaterialIcon role="button" icon="search"/>}
+              >
+              <Input
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)} />
+            </TextField>
+            <CarouselTitle>Na seu Área</CarouselTitle>
+            <Carousel {...settings}>
+              <Card photo={restaurant} title="Restaurant name" />
+              <Card photo={restaurant} title="Restaurant name" />
+              <Card photo={restaurant} title="Restaurant name" />
+              <Card photo={restaurant} title="Restaurant name" />
+              <Card photo={restaurant} title="Restaurant name" />
+              <Card photo={restaurant} title="Restaurant name" />
+              <Card photo={restaurant} title="Restaurant name" />
+              <Card photo={restaurant} title="Restaurant name" />
+            </Carousel>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
  )
 };
